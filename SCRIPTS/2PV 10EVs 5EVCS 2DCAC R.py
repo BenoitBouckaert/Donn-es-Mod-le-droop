f1251edeca70640387e29b747bd2368e0c8df3dd
Benoit <benoit.bouckaert@junia.com>
2024-02-19 10:35:17 +0100

    More than 1-day raw data Fixes #9 Implements a proper way to manage date (real calculation of dates instead of hard-assigning dates)

--- SCRIPTS/2PV 10EVs 5EVCS 2DCAC R.py
+++ SCRIPTS/2PV 10EVs 5EVCS 2DCAC R.py
@@ -8,12 +8,15 @@ RAW_FILE_PATH = st.RAW_DATA_PATH + FILE_NAME
 PROCESSED_FILE_PATH = st.PROCESSED_DATA_PATH + FILE_NAME
 
 # Processes the raw data and saves it in the processed data folder
-process_csv(RAW_FILE_PATH, PROCESSED_FILE_PATH)
+process_csv(RAW_FILE_PATH, PROCESSED_FILE_PATH, start_time='05:00:00', date='2022-06-19')
 
 # Plots (from base data) :
 # 1. 'U_DC1' and 'U_DC2' vs 'Time' as a line graph
 plot(PROCESSED_FILE_PATH, ['Time'], ['U_DC'], type='line', title='U_DC vs Time', 
      Y_label='bus Voltage', Y_unit='V', X_label='Time', style='display')
+
+plot(PROCESSED_FILE_PATH, ['Time'], ['P_PV1', 'P_PV2'], type='line', title='P_PV1 + P_PV2 vs Time', 
+     Y_label='Power', Y_unit='W', X_label='Time', style='display', legend=['$P_{PV1}$', '$P_{PV2}$'])
 
 # 2. 'P_acdc1' + 'P_acdc2, 'P_PV1' + 'P_PV2', and 'P_EVs' vs 'Time' as a line graph
 plot(PROCESSED_FILE_PATH, ['Time'], ['P_acdc1', 'P_acdc2', 'P_PV1', 'P_PV2', 'P_EVs'], 
